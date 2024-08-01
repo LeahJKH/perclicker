204741b7211f26a4eeb31387605a58f9ae2468b7
LeahJKH <leahjb06.kodehode@gmail.com>
2024-08-01 09:29:21 +0200

    cleaned up a bit

--- Js/app.js
+++ Js/app.js
@@ -1,11 +1,12 @@
-let moneyEl = document.getElementById("money-el");
-const audio = new Audio("./audio/encourage.mp3");
-const transBg = document.querySelector("#trans-bg")
-const overMenu = document.querySelector("#over-Menu")
-const closeMenu = document.querySelector("#close")
-const items = document.querySelector("#items")
-const HeadTxt = document.querySelector("#headTxt")
-
+const ObjP = {
+  moneyEl: document.querySelector("#money-el"),
+  audio: new Audio("./audio/encourage.mp3"),
+  transBg: document.querySelector("#trans-bg"),
+  overMenu: document.querySelector("#over-Menu"),
+  closeMenu: document.querySelector("#close"),
+  items: document.querySelector("#items"),
+  HeadTxt: document.querySelector("#headTxt")
+}
 let user = {
   name: "",
   money: 0,
@@ -26,12 +27,12 @@ localStorage.setItem("cash", user.money);
 function startgame() {
   user.upgraded = Number(localStorage.getItem("multiplier"));
   user.money = Number(localStorage.getItem("cash"));
-  moneyEl.textContent = user.money;
+  ObjP.moneyEl.textContent = user.money;
 }
 
 function increment() {
   user.money += user.upgraded;
-  moneyEl.textContent = user.money;
+  ObjP.moneyEl.textContent = user.money;
   localStorage.setItem("cash", user.money);
 
   startgame();
@@ -40,7 +41,7 @@ function increment() {
 function buyUp(num) {
   if (num = 4) {
     if(AudioSwitch === true) {
-      audio.play()
+      ObjP.audio.play()
     } else {
       return
     }
@@ -63,33 +64,33 @@ function clearCache() {
 }
 
 closeMenu.addEventListener("click", () => {
-  transBg.style.display = "none"
-  items.innerHTML = ``
+  ObjP.transBg.style.display = "none"
+  ObjP.items.innerHTML = ``
 })
 function resetNum() {
   localStorage.getItem("multiplier", user.upgraded);
 }
 
 function bugrepo() {
-  HeadTxt.textContent = "bug report"
-  transBg.style.display = "flex";
+  ObjP.HeadTxt.textContent = "bug report"
+  ObjP.transBg.style.display = "flex";
   const p = document.createElement("p")
   const Stxt = document.createTextNode("if you wanna report a bug u deffo have me on discord.")
   p.append(Stxt)
-  items.append(p)
+  ObjP.items.append(p)
 }
 
 function forPer() {
-  HeadTxt.textContent = "message for per"
-  transBg.style.display = "flex";
+  ObjP.HeadTxt.textContent = "message for per"
+  ObjP.transBg.style.display = "flex";
   const p = document.createElement("p")
   const Stxt = document.createTextNode("this project was made because of my love for Per and his amazing abilites.")
   p.append(Stxt)
-  items.append(p)
+  ObjP.items.append(p)
 }
 function inDev() {
-  HeadTxt.textContent = "Settings"
-  transBg.style.display = "flex";
+  ObjP.HeadTxt.textContent = "Settings"
+  ObjP.transBg.style.display = "flex";
   const h1 = document.createElement("h1")
   const Stxt = document.createTextNode("Settings")
   const audioCheck = document.createElement("input")
@@ -105,9 +106,9 @@ function inDev() {
   const ALTxt = document.createTextNode("Audio")
   audioLabel.append(ALTxt)
   h1.append(Stxt)
-  items.append(h1)
-  items.append(audioCheck)
-  items.append(audioLabel)
+  ObjP.items.append(h1)
+  ObjP.items.append(audioCheck)
+  ObjP.items.append(audioLabel)
   audioCheck.addEventListener("click", () => {
     AudioSwitch = !AudioSwitch
   })
